4835809cd84259a913035457d1f8e4a39fc995e2
ozdesy <y.ozdes@live.nl>
2020-03-17 21:02:50 +0100

    opdracht 2

--- opdracht2/js/script.js
+++ opdracht2/js/script.js
@@ -1,3 +1,18 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 /*eslint-env browser*/
 /*eslint 'no-console': 0*/
+
+/*********** VIDEO ALS VOORBEELD GENOMEN LINK: https://www.youtube.com/watch?v=drOgpionKpY ************/
+
+$(document).ready(function () {
+    $('#autoWidth').lightSlider({
+        autoWidth: true,
+        loop: true,
+        onSliderLoad: function () {
+            $('#autoWidth').removeClass('cS-hidden');
+        }
+    });
+});
+
+
+/* enige wat niet is gelukt zijn de pijltjes die naar links en rechts wijzen */
